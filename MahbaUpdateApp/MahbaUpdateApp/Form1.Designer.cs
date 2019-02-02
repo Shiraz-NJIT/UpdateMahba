8d7bc3457586fabaef82552ef4411685e3a3aa9b
mohammad <mh3rahmani@gmail.com>
2019-02-02 20:58:12 +0330

    برنامه آپدیت برنامه مهبا
    برنامه آپدیت برنامه مهبا

---
+++ MahbaUpdateApp/MahbaUpdateApp/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MahbaUpdateApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.bunifuCircleProgressbar1 = new ns1.BunifuCircleProgressbar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(126, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "برنامه در حال بروز رسانی است لطفا صبور باشید";
+            // 
+            // bunifuCircleProgressbar1
+            // 
+            this.bunifuCircleProgressbar1.animated = false;
+            this.bunifuCircleProgressbar1.animationIterval = 5;
+            this.bunifuCircleProgressbar1.animationSpeed = 300;
+            this.bunifuCircleProgressbar1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuCircleProgressbar1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuCircleProgressbar1.BackgroundImage")));
+            this.bunifuCircleProgressbar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.bunifuCircleProgressbar1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.bunifuCircleProgressbar1.LabelVisible = false;
+            this.bunifuCircleProgressbar1.LineProgressThickness = 8;
+            this.bunifuCircleProgressbar1.LineThickness = 5;
+            this.bunifuCircleProgressbar1.Location = new System.Drawing.Point(70, 12);
+            this.bunifuCircleProgressbar1.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.bunifuCircleProgressbar1.MaxValue = 100;
+            this.bunifuCircleProgressbar1.Name = "bunifuCircleProgressbar1";
+            this.bunifuCircleProgressbar1.ProgressBackColor = System.Drawing.Color.Silver;
+            this.bunifuCircleProgressbar1.ProgressColor = System.Drawing.Color.GreenYellow;
+            this.bunifuCircleProgressbar1.Size = new System.Drawing.Size(221, 221);
+            this.bunifuCircleProgressbar1.TabIndex = 1;
+            this.bunifuCircleProgressbar1.UseWaitCursor = true;
+            this.bunifuCircleProgressbar1.Value = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(388, 27);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.bunifuCircleProgressbar1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(388, 242);
+            this.panel2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(141, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "لطفا منتظر بمانید";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(388, 269);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private ns1.BunifuCircleProgressbar bunifuCircleProgressbar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MahbaUpdateApp/MahbaUpdateApp/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MahbaUpdateApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Model.NjitEntities db = new Model.NjitEntities();
+
+        public static string GetMacAddress()
+        {
+            string hostName = System.Net.Dns.GetHostName(); // Retrive the Name of HOST  
+            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            return myIP;
+        }
+        
+        private void DeleteAllFile()
+        {
+            string macaddress = GetMacAddress();
+            string PathApp = db.VersionClients.Where(a => a.ClientIP == macaddress).OrderByDescending(a => a.ID).FirstOrDefault().MashinPath;
+
+            System.IO.DirectoryInfo di = new DirectoryInfo(PathApp);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+        private void UpdateApplication()
+        {
+            try
+            {
+                #region گرفتن فایل های جدید از سرور که فایل های آن درون سرور ریختیم
+                string NewSourcePathApp = db.ProgramSettings.FirstOrDefault().AppUpdatePathExe;
+                #endregion
+                #region آدرس برنامه ورژن قدیمی که روی سیستم کاربر نصب است بر اساس ای پی سیستم میگیرد
+                string macaddress = GetMacAddress();
+                string PathApp = db.VersionClients.Where(a => a.ClientIP == macaddress).OrderByDescending(a => a.ID).FirstOrDefault().MashinPath;
+                #endregion
+
+                foreach (string dirPath in Directory.GetDirectories(NewSourcePathApp, "*", SearchOption.AllDirectories))
+                    Directory.CreateDirectory(dirPath.Replace(NewSourcePathApp, PathApp));
+                //Copy all the files & Replaces any files with the same name
+                foreach (string newPath in Directory.GetFiles(NewSourcePathApp, "*.*", SearchOption.AllDirectories))
+                    File.Copy(newPath, newPath.Replace(NewSourcePathApp, PathApp), true);
+                #region MyRegion
+
+                //var versionApp = db.VesionApps.Where(a => a.OldVersion != a.NewVersion).FirstOrDefault();
+                //versionApp.OldVersion = versionApp.NewVersion;
+                //versionApp.CreateDate = DateTime.Now;
+                //db.SaveChanges();
+                //var Lastclient = db.VersionClients.Where(a => a.ClientIP == macaddress).OrderByDescending(a => a.ID).FirstOrDefault();
+                //var List = db.VersionClients.Where(a => a.ClientIP == macaddress).ToList();
+                //db.VersionClients.RemoveRange(List);
+                //db.SaveChanges();
+
+                //db.VersionClients.Add(new Model.VersionClient
+                //{
+                //    ClientIP = Lastclient.ClientIP,
+                //    IsInstallUpdate = true,
+                //    MashinPath = Lastclient.MashinPath,
+                //    userId = Lastclient.userId
+                //});
+                //db.SaveChanges();
+                #endregion
+                #region اجرا شدن برنامه روی سیستم کاربر بعد از تمام شدن کپی فایل های جدید
+                string MahbaApp = db.VersionClients.Where(a => a.ClientIP == macaddress).OrderByDescending(a => a.ID).FirstOrDefault().MashinPath + "Mahba.exe";
+                System.Diagnostics.Process.Start(MahbaApp);
+                #endregion
+
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                var Result = MessageBox.Show(ex.Message, "خطا!!! لطفا متن خطا به پشتیبانی ارسال نمایید");
+                Application.Exit();
+            }
+
+        }
+        bool isStart = true;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (bunifuCircleProgressbar1.Value == 50)
+            {
+                if (isStart)
+                {
+                    isStart = false;
+                    UpdateApplication();
+                }
+                bunifuCircleProgressbar1.Value += 1;
+            }
+            else if (bunifuCircleProgressbar1.Value < bunifuCircleProgressbar1.MaxValue)
+            {
+                bunifuCircleProgressbar1.Value += 1;
+            }
+            else
+            {
+                bunifuCircleProgressbar1.Value = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateApplication();
+        }
+    }
+}
